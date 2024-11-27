0df478bd4218e8d079e14439fe4cafb50836e5a7
b-a-dehaan <dehaanbethany3@gmail.com>
2024-11-27 15:33:21 -0500

    Added buoyancy and Wave logic
    Actual waves tbd

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public float amplitude = 1.0f;
+    public float length = 2.0f;
+    public float speed = 1.0f;
+    public float offset = 0.0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object");
+            Destroy(this);
+        }
+
+    }
+    private void Update()
+    {
+        offset += Time.deltaTime * speed;
+    }
+    public float GetWaveHeight(float x)
+    {
+        return amplitude * Mathf.Sin(x / length + offset);
+    }
+}

ca6899e2b9ffa195b0c9e0207ee70c9180489ed6
b-a-dehaan <dehaanbethany3@gmail.com>
2024-11-27 19:51:32 -0500

    touch ups

--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -8,7 +8,9 @@ public class Floating : MonoBehaviour
     public Rigidbody rb;
     public float depthBeforeSubmerged = 2f;
     public float displacementAmount = 3f;
-    public int floaterCount = 1;
+    public int floaterCount = 4;
+    public float waterDrag = 0.01f;
+    public float waterAngularDrag = 0.01f;
 
     private void FixedUpdate()
     {
@@ -19,6 +21,8 @@ public class Floating : MonoBehaviour
         {
             float displacementMultiplyer = Mathf.Clamp01 ((waveHeight - transform.position.y  )/ depthBeforeSubmerged) * displacementAmount;
             rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplyer, 0f) , transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultiplyer * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultiplyer * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
     }
 }
